callstack/react-native-paper

commit cb5b9b43d4a8c77e9f04c1d94c0c1814bbac7d8c

refactor: update appBar class components to functional (#2179)

src/components/Appbar/AppbarHeader.tsx
@@ -75,58 +75,53 @@ type Props = React.ComponentProps<typeof Appbar> & {
  * export default MyComponent;
  * ```
  */
-class AppbarHeader extends React.Component<Props> {
-  static displayName = 'Appbar.Header';
+const AppbarHeader = ({
+  // Don't use default props since we check it to know whether we should use SafeAreaView
+  statusBarHeight = APPROX_STATUSBAR_HEIGHT,
+  style,
+  dark,
+  ...rest
+}: Props) => {
+  const { dark: isDarkTheme, colors, mode } = rest.theme;
+  const {
+    height = DEFAULT_APPBAR_HEIGHT,
+    elevation = 4,
+    backgroundColor: customBackground,
+    zIndex = 0,
+    ...restStyle
+  }: ViewStyle = StyleSheet.flatten(style) || {};
+  const backgroundColor = customBackground
+    ? customBackground
+    : isDarkTheme && mode === 'adaptive'
+    ? overlay(elevation, colors.surface)
+    : colors.primary;
+  // Let the user override the behaviour
+  const Wrapper = typeof statusBarHeight === 'number' ? View : SafeAreaView;
 
-  render() {
-    const {
-      // Don't use default props since we check it to know whether we should use SafeAreaView
-      statusBarHeight = APPROX_STATUSBAR_HEIGHT,
-      style,
-      dark,
-      ...rest
-    } = this.props;
-    const { dark: isDarkTheme, colors, mode } = rest.theme;
-    const {
-      height = DEFAULT_APPBAR_HEIGHT,
-      elevation = 4,
-      backgroundColor: customBackground,
-      zIndex = 0,
-      ...restStyle
-    }: ViewStyle = StyleSheet.flatten(style) || {};
-    const backgroundColor = customBackground
-      ? customBackground
-      : isDarkTheme && mode === 'adaptive'
-      ? overlay(elevation, colors.surface)
-      : colors.primary;
-    // Let the user override the behaviour
-    const Wrapper =
-      typeof this.props.statusBarHeight === 'number' ? View : SafeAreaView;
+  return (
+    <Wrapper
+      style={
+        [
+          { backgroundColor, zIndex, elevation },
+          shadow(elevation),
+        ] as StyleProp<ViewStyle>
+      }
+    >
+      <Appbar
+        style={[
+          //@ts-ignore Types of property 'backgroundColor' are incompatible.
+          { height, backgroundColor, marginTop: statusBarHeight },
+          styles.appbar,
+          restStyle,
+        ]}
+        dark={dark}
+        {...rest}
+      />
+    </Wrapper>
+  );
+};
 
-    return (
-      <Wrapper
-        style={
-          [
-            { backgroundColor, zIndex, elevation },
-            shadow(elevation),
-          ] as StyleProp<ViewStyle>
-        }
-      >
-        <Appbar
-          //@ts-ignore
-          style={[
-            //@ts-ignore
-            { height, backgroundColor, marginTop: statusBarHeight },
-            styles.appbar,
-            restStyle,
-          ]}
-          dark={dark}
-          {...rest}
-        />
-      </Wrapper>
-    );
-  }
-}
+AppbarHeader.displayName = 'Appbar.Header';
 
 const styles = StyleSheet.create({
   appbar: {

src/components/Appbar/Appbar.tsx
@@ -78,113 +78,109 @@ export const DEFAULT_APPBAR_HEIGHT = 56;
  * });
  * ```
  */
-class Appbar extends React.Component<Props> {
-  // @component ./AppbarContent.tsx
-  static Content = AppbarContent;
-  // @component ./AppbarAction.tsx
-  static Action = AppbarAction;
-  // @component ./AppbarBackAction.tsx
-  static BackAction = AppbarBackAction;
-  // @component ./AppbarHeader.tsx
-  static Header = AppbarHeader;
+const Appbar = ({ children, dark, style, theme, ...rest }: Props) => {
+  const { colors, dark: isDarkTheme, mode } = theme;
+  const {
+    backgroundColor: customBackground,
+    elevation = 4,
+    ...restStyle
+  }: ViewStyle = StyleSheet.flatten(style) || {};
 
-  render() {
-    const { children, dark, style, theme, ...rest } = this.props;
+  let isDark: boolean;
 
-    const { colors, dark: isDarkTheme, mode } = theme;
-    const {
-      backgroundColor: customBackground,
-      elevation = 4,
-      ...restStyle
-    }: ViewStyle = StyleSheet.flatten(style) || {};
-
-    let isDark: boolean;
-
-    const backgroundColor = customBackground
-      ? customBackground
-      : isDarkTheme && mode === 'adaptive'
-      ? overlay(elevation, colors.surface)
-      : colors.primary;
-    if (typeof dark === 'boolean') {
-      isDark = dark;
-    } else {
-      isDark =
-        backgroundColor === 'transparent'
-          ? false
-          : !color(backgroundColor).isLight();
-    }
-
-    let shouldCenterContent = false;
-    let shouldAddLeftSpacing = false;
-    let shouldAddRightSpacing = false;
-    if (Platform.OS === 'ios') {
-      let hasAppbarContent = false;
-      let leftItemsCount = 0;
-      let rightItemsCount = 0;
-
-      React.Children.forEach(children, (child) => {
-        if (React.isValidElement(child)) {
-          if (child.type === AppbarContent) {
-            hasAppbarContent = true;
-          } else if (hasAppbarContent) {
-            rightItemsCount++;
-          } else {
-            leftItemsCount++;
-          }
-        }
-      });
-
-      shouldCenterContent =
-        hasAppbarContent && leftItemsCount < 2 && rightItemsCount < 2;
-      shouldAddLeftSpacing = shouldCenterContent && leftItemsCount === 0;
-      shouldAddRightSpacing = shouldCenterContent && rightItemsCount === 0;
-    }
-    return (
-      <Surface
-        //@ts-ignore
-        style={[{ backgroundColor }, styles.appbar, { elevation }, restStyle]}
-        {...rest}
-      >
-        {shouldAddLeftSpacing ? <View style={styles.spacing} /> : null}
-        {React.Children.toArray(children)
-          .filter((child) => child != null && typeof child !== 'boolean')
-          .map((child, i) => {
-            if (
-              !React.isValidElement(child) ||
-              ![
-                AppbarContent,
-                AppbarAction,
-                AppbarBackAction,
-                // @ts-ignore
-              ].includes(child.type)
-            ) {
-              return child;
-            }
-
-            const props: { color?: string; style?: StyleProp<ViewStyle> } = {
-              color:
-                typeof child.props.color !== 'undefined'
-                  ? child.props.color
-                  : isDark
-                  ? white
-                  : black,
-            };
-
-            if (child.type === AppbarContent) {
-              props.style = [
-                // Since content is not first item, add extra left margin
-                i !== 0 && { marginLeft: 8 },
-                shouldCenterContent && { alignItems: 'center' },
-                child.props.style,
-              ];
-            }
-            return React.cloneElement(child, props);
-          })}
-        {shouldAddRightSpacing ? <View style={styles.spacing} /> : null}
-      </Surface>
-    );
+  const backgroundColor = customBackground
+    ? customBackground
+    : isDarkTheme && mode === 'adaptive'
+    ? overlay(elevation, colors.surface)
+    : colors.primary;
+  if (typeof dark === 'boolean') {
+    isDark = dark;
+  } else {
+    isDark =
+      backgroundColor === 'transparent'
+        ? false
+        : !color(backgroundColor).isLight();
   }
-}
+
+  let shouldCenterContent = false;
+  let shouldAddLeftSpacing = false;
+  let shouldAddRightSpacing = false;
+  if (Platform.OS === 'ios') {
+    let hasAppbarContent = false;
+    let leftItemsCount = 0;
+    let rightItemsCount = 0;
+
+    React.Children.forEach(children, (child) => {
+      if (React.isValidElement(child)) {
+        if (child.type === AppbarContent) {
+          hasAppbarContent = true;
+        } else if (hasAppbarContent) {
+          rightItemsCount++;
+        } else {
+          leftItemsCount++;
+        }
+      }
+    });
+
+    shouldCenterContent =
+      hasAppbarContent && leftItemsCount < 2 && rightItemsCount < 2;
+    shouldAddLeftSpacing = shouldCenterContent && leftItemsCount === 0;
+    shouldAddRightSpacing = shouldCenterContent && rightItemsCount === 0;
+  }
+  return (
+    <Surface
+      //@ts-ignore Types of property 'backgroundColor' are incompatible.
+      style={[{ backgroundColor }, styles.appbar, { elevation }, restStyle]}
+      {...rest}
+    >
+      {shouldAddLeftSpacing ? <View style={styles.spacing} /> : null}
+      {React.Children.toArray(children)
+        .filter((child) => child != null && typeof child !== 'boolean')
+        .map((child, i) => {
+          if (
+            !React.isValidElement(child) ||
+            ![
+              AppbarContent,
+              AppbarAction,
+              AppbarBackAction,
+              // @ts-ignore Type 'string' is not assignable to type
+            ].includes(child.type)
+          ) {
+            return child;
+          }
+
+          const props: { color?: string; style?: StyleProp<ViewStyle> } = {
+            color:
+              typeof child.props.color !== 'undefined'
+                ? child.props.color
+                : isDark
+                ? white
+                : black,
+          };
+
+          if (child.type === AppbarContent) {
+            props.style = [
+              // Since content is not first item, add extra left margin
+              i !== 0 && { marginLeft: 8 },
+              shouldCenterContent && { alignItems: 'center' },
+              child.props.style,
+            ];
+          }
+          return React.cloneElement(child, props);
+        })}
+      {shouldAddRightSpacing ? <View style={styles.spacing} /> : null}
+    </Surface>
+  );
+};
+
+// @component ./AppbarContent.tsx
+Appbar.Content = AppbarContent;
+// @component ./AppbarAction.tsx
+Appbar.Action = AppbarAction;
+// @component ./AppbarBackAction.tsx
+Appbar.BackAction = AppbarBackAction;
+// @component ./AppbarHeader.tsx
+Appbar.Header = AppbarHeader;
 
 const styles = StyleSheet.create({
   appbar: {

src/components/Appbar/AppbarAction.tsx
@@ -73,33 +73,27 @@ type Props = React.ComponentPropsWithoutRef<typeof IconButton> & {
  * export default MyComponent;
  * ```
  */
-export default class AppbarAction extends React.Component<Props> {
-  static displayName = 'Appbar.Action';
+const AppbarAction = ({
+  size = 24,
+  color: iconColor = color(black).alpha(0.54).rgb().string(),
+  icon,
+  disabled,
+  onPress,
+  accessibilityLabel,
+  ...rest
+}: Props) => (
+  <IconButton
+    size={size}
+    onPress={onPress}
+    color={iconColor}
+    icon={icon}
+    disabled={disabled}
+    accessibilityLabel={accessibilityLabel}
+    animated
+    {...rest}
+  />
+);
 
-  static defaultProps = {
-    size: 24,
-  };
+AppbarAction.displayName = 'Appbar.Action';
 
-  render() {
-    const {
-      color: iconColor = color(black).alpha(0.54).rgb().string(),
-      icon,
-      disabled,
-      onPress,
-      accessibilityLabel,
-      ...rest
-    } = this.props;
-
-    return (
-      <IconButton
-        onPress={onPress}
-        color={iconColor}
-        icon={icon}
-        disabled={disabled}
-        accessibilityLabel={accessibilityLabel}
-        animated
-        {...rest}
-      />
-    );
-  }
-}
+export default AppbarAction;

src/components/Appbar/AppbarBackAction.tsx
@@ -63,16 +63,14 @@ type Props = $Omit<
  * export default MyComponent;
  * ```
  */
-class AppbarBackAction extends React.Component<Props> {
-  static displayName = 'Appbar.BackAction';
+const AppbarBackAction = ({ accessibilityLabel = 'Back', ...rest }: Props) => (
+  <AppbarAction
+    accessibilityLabel={accessibilityLabel}
+    {...rest}
+    icon={AppbarBackIcon}
+  />
+);
 
-  static defaultProps = {
-    accessibilityLabel: 'Back',
-  };
-
-  render() {
-    return <AppbarAction {...this.props} icon={AppbarBackIcon} />;
-  }
-}
+AppbarBackAction.displayName = 'Appbar.BackAction';
 
 export default AppbarBackAction;

src/components/Appbar/AppbarContent.tsx
@@ -76,60 +76,57 @@ type Props = $RemoveChildren<typeof View> & {
  * export default MyComponent;
  * ```
  */
-class AppbarContent extends React.Component<Props> {
-  static displayName = 'Appbar.Content';
+const AppbarContent = ({
+  color: titleColor = white,
+  subtitle,
+  subtitleStyle,
+  onPress,
+  style,
+  titleRef,
+  titleStyle,
+  theme,
+  title,
+  ...rest
+}: Props) => {
+  const { fonts } = theme;
 
-  render() {
-    const {
-      color: titleColor = white,
-      subtitle,
-      subtitleStyle,
-      onPress,
-      style,
-      titleRef,
-      titleStyle,
-      theme,
-      title,
-      ...rest
-    } = this.props;
-    const { fonts } = theme;
+  const subtitleColor = color(titleColor).alpha(0.7).rgb().string();
 
-    const subtitleColor = color(titleColor).alpha(0.7).rgb().string();
-
-    return (
-      <TouchableWithoutFeedback onPress={onPress} disabled={!onPress}>
-        <View style={[styles.container, style]} {...rest}>
+  return (
+    <TouchableWithoutFeedback onPress={onPress} disabled={!onPress}>
+      <View style={[styles.container, style]} {...rest}>
+        <Text
+          ref={titleRef}
+          style={[
+            {
+              color: titleColor,
+              ...(Platform.OS === 'ios' ? fonts.regular : fonts.medium),
+            },
+            styles.title,
+            titleStyle,
+          ]}
+          numberOfLines={1}
+          accessible
+          accessibilityTraits="header"
+          // @ts-ignore Type '"heading"' is not assignable to type ...
+          accessibilityRole={Platform.OS === 'web' ? 'heading' : 'header'}
+        >
+          {title}
+        </Text>
+        {subtitle ? (
           <Text
-            ref={titleRef}
-            style={[
-              {
-                color: titleColor,
-                ...(Platform.OS === 'ios' ? fonts.regular : fonts.medium),
-              },
-              styles.title,
-              titleStyle,
-            ]}
+            style={[styles.subtitle, { color: subtitleColor }, subtitleStyle]}
             numberOfLines={1}
-            accessible
-            accessibilityTraits="header"
-            // @ts-ignore
-            accessibilityRole={Platform.OS === 'web' ? 'heading' : 'header'}
           >
-            {title}
+            {subtitle}
           </Text>
-          {subtitle ? (
-            <Text
-              style={[styles.subtitle, { color: subtitleColor }, subtitleStyle]}
-              numberOfLines={1}
-            >
-              {subtitle}
-            </Text>
-          ) : null}
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-}
+        ) : null}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+AppbarContent.displayName = 'Appbar.Content';
 
 const styles = StyleSheet.create({
   container: {

crazymobdev/react_native_paper

commit 6153e6284b167025fc033329890813f1b1b6befa

fix: fix appbar regression made in https://github.com/callstack/react-native-paper/pull/2179

src/components/Appbar/AppbarHeader.tsx
@@ -75,13 +75,15 @@ type Props = React.ComponentProps<typeof Appbar> & {
  * export default MyComponent;
  * ```
  */
-const AppbarHeader = ({
-  // Don't use default props since we check it to know whether we should use SafeAreaView
-  statusBarHeight = APPROX_STATUSBAR_HEIGHT,
-  style,
-  dark,
-  ...rest
-}: Props) => {
+const AppbarHeader = (props: Props) => {
+  const {
+    // Don't use default props since we check it to know whether we should use SafeAreaView
+    statusBarHeight = APPROX_STATUSBAR_HEIGHT,
+    style,
+    dark,
+    ...rest
+  } = props;
+
   const { dark: isDarkTheme, colors, mode } = rest.theme;
   const {
     height = DEFAULT_APPBAR_HEIGHT,
@@ -96,8 +98,8 @@ const AppbarHeader = ({
     ? overlay(elevation, colors.surface)
     : colors.primary;
   // Let the user override the behaviour
-  const Wrapper = typeof statusBarHeight === 'number' ? View : SafeAreaView;
-
+  const Wrapper =
+    typeof props.statusBarHeight === 'number' ? View : SafeAreaView;
   return (
     <Wrapper
       style={